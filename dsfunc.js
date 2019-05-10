e4f16d461bd0a7befea1293e8957212d151bbf13
Ravi.Katiyar <ravi.katiyar@naukri.com>
2019-05-10 09:25:19 +0530

    added the changes to reverse an array

--- dsfunc.js
+++ dsfunc.js
@@ -1,3 +1,16 @@
 function runmycode(){
-    document.getElementById('change').innerHTML = 'I was clicked';
+    document.getElementById('change').innerHTML = 3;  
+
+}
+
+function arrreverse(){
+    var arr = [1,2,3,4,5,6,7,8,9,10];
+    var arr2 = new Array; 
+    var j = 0;
+    for (var i = arr.length -1; i>= 0; i--){
+      arr2[j] = arr[i];
+        j++;
+    }
+    document.getElementById('change').innerHTML = arr2;
+    
 }
