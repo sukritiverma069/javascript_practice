ddcc673793084c008e112fb3e64448108a33aad3
sukritiverma069 <sukritiverma069@gmail.com>
2019-05-12 23:21:08 +0530

    array reversal by taking html input

--- dsfunc.js
+++ dsfunc.js
@@ -4,7 +4,10 @@ function runmycode(){
 }
 
 function arrreverse(){
-    var arr = [1,2,3,4,5,6,7,8,9,10];
+    //var arr = [1,2,3,4,5,6,7,8,9,10];()
+    var inputText = document.getElementById("arraybox").value;
+    var arr = inputText.split(',');
+    
     var arr2 = new Array; 
     var j = 0;
     for (var i = arr.length -1; i>= 0; i--){
