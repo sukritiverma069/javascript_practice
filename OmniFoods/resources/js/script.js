8302d723c5a74533619f9713569c577d07e6a7c5
sukritiverma069 <sukritiverma069@gmail.com>
2019-07-29 23:33:00 +0530

    optimization in progress

--- OmniFoods/resources/js/script.js
+++ OmniFoods/resources/js/script.js
@@ -89,11 +89,20 @@ $(document).ready(function() {
 
 
   /*-----------MOBILE-NAV---------- */
-  $('js--nav-icon').click(function(){
+  $('.js--nav-icon').click(function(){
 
-    var nav = $('js--main-nav');
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon');
 
-    nav.slideToggle(200)
+    nav.slideToggle(200);
+    if(icon.hasClass('ion-navicon-round')){
+      icon.addClass('ion-close-round');
+      icon-removeClass('ion-navicon-round');
+    }else{
+      icon.addClass('ion-navicon-round');
+      icon-removeClass('ion-close-round');
+    }
+    
   });
 
 
