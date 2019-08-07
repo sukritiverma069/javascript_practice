a3f0825fa955b76d1c0276150e6560e39cde06e6
Sukriti Verma <sukritiverma069@gmail.com>
2019-08-07 11:50:15 +0530

    omnifood added as a hyperlink

--- 99acres_project/js/script.js
+++ 99acres_project/js/script.js
@@ -3,7 +3,7 @@ function getLoginStatus(){
     var password = document.getElementById("password").value;
     var anchor_tag = document.getElementById("btn");
     if(username == "Sukriti" && password == "abcd123"){
-        anchor_tag.href = "http://www.99acres.com";
+        anchor_tag.href = "Omnifoods/index.html";
     }else {
         anchor_tag.href = "../error.html";
     }
