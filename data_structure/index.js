d3499dd0d31f2a4d11dfbf5935723c75b8346a40
sukritiverma069 <50458910+sukritiverma069@users.noreply.github.com>
2020-06-24 12:37:49 +0530

    Update index.js

--- data_structure/index.js
+++ data_structure/index.js
@@ -12,6 +12,17 @@ function getArray(){
       
    }
 
+const factorial = (num) => {
+    var result = num;
+    if (num === 0 || num === 1) return 1;
+    while (num > 1) {
+      num--;
+      result *= num;
+    }
+    console.log(result);
+    return result;
+  };
+
   const stringReverse = (str) => {
     var newString = "";
     for (var i = str.length - 1; i >= 0; i--) {
