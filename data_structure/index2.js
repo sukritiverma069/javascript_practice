62bb6ea603abf60ee865ae725ad91f3f6ea23719
sukritiverma069 <50458910+sukritiverma069@users.noreply.github.com>
2020-10-15 15:50:52 +0530

    Update index2.js

--- data_structure/index2.js
+++ data_structure/index2.js
@@ -46,3 +46,50 @@ return arr.join('');
 
 }
 
+
+//convert obj to an array
+
+function toArray(obj) {
+	
+	let arr = []
+	for(let [key, value] of Object.entries(obj)){
+	arr.push([key,value])
+}
+	return arr
+}
+
+
+//sort an array of strings on the basis of length of each of the string
+//let a = ["a", "ccc", "dddd", "bb"]
+function sortByLength(arr) {
+	return arr.sort((a,b)=> { a.length-b.length})
+}
+
+//A boomerang is a V-shaped sequence that is either upright or upside down.
+// Specifically, a boomerang can be defined as: sub-array of length 3, with the first and last digits being the same and the middle digit being different.
+// Create a function that returns the total number of boomerangs in an array.
+//[1, 7, 1, 7, 1, 7, 1]
+// 5 boomerangs (from left to right): [1, 7, 1], [7, 1, 7], [1, 7, 1], [7, 1, 7], and [1, 7, 1]
+
+
+function countBoomerangs(arr) {
+	let start = 0
+	let end = 2
+	let count=0
+	
+	while(end <= arr.length-1){
+		
+		if(arr[start]===arr[end] && arr[start+1]!== arr[start]){
+			count++
+		}
+		start++
+		end++
+	}
+	
+	return count
+}
+
+
+
+
+
